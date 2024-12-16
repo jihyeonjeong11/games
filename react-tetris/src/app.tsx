1773e566507724cc367ad7194f41904701167dcf
jihyeonjeong11 <wjdwlgus11@gmail.com>
2024-12-16 16:23:01 +0900

    drawing canvas with tetrisboard

--- react-tetris/src/app.tsx
+++ react-tetris/src/app.tsx
@@ -1,43 +1,49 @@
-import { useState } from 'preact/hooks'
-import preactLogo from './assets/preact.svg'
-import viteLogo from '/vite.svg'
-import './app.css'
+import { useEffect, useRef } from "preact/hooks";
+import "./app.css";
+
+const rows = 20;
+const cols = 10;
+const cellSize = 20;
 
 export function App() {
-  const [count, setCount] = useState(0)
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) {
+      return;
+    }
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    canvas.width = cols * cellSize;
+    canvas.height = rows * cellSize;
+    ctx.strokeStyle = "#ccc";
+    ctx.lineWidth = 0.5;
+
+    // vertical lines
+    for (let x = 0; x <= cols; x++) {
+      ctx.beginPath();
+      ctx.moveTo(x * cellSize, 0);
+      ctx.lineTo(x * cellSize, rows * cellSize);
+      ctx.stroke();
+    }
+
+    // horizontal lines
+    for (let y = 0; y <= rows; y++) {
+      ctx.beginPath();
+      ctx.moveTo(0, y * cellSize);
+      ctx.lineTo(cols * cellSize, y * cellSize);
+      ctx.stroke();
+    }
+  }, []);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://preactjs.com" target="_blank">
-          <img src={preactLogo} class="logo preact" alt="Preact logo" />
-        </a>
-      </div>
       <h1>Vite + Preact</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/app.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p>
-        Check out{' '}
-        <a
-          href="https://preactjs.com/guide/v10/getting-started#create-a-vite-powered-preact-app"
-          target="_blank"
-        >
-          create-preact
-        </a>
-        , the official Preact + Vite starter
-      </p>
-      <p class="read-the-docs">
-        Click on the Vite and Preact logos to learn more
-      </p>
+      <canvas
+        ref={canvasRef}
+        width={rows * cellSize}
+        height={cols * cellSize}
+      />
     </>
-  )
+  );
 }
